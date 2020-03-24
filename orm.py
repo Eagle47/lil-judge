67a7ef24b493748ea942d287f2ed2eec0794c78d
Alexey Ermakov <fufler@gmail.com>
2020-03-24 12:08:33 +0300

    Update DB tables structure

--- orm.py
+++ orm.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Boolean, Integer, String, UniqueConstraint, ForeignKey, DateTime
+from sqlalchemy import Column, Boolean, Integer, String, UniqueConstraint, ForeignKey, DateTime, LargeBinary, JSON
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -28,8 +28,61 @@ class Contest(Base):
     name = Column(String)
     start_date = Column(DateTime(timezone=True))
     finish_date = Column(DateTime(timezone=True))
+    paused = Column(Boolean, default=False)
 
     members = relationship('ContestMember')
+    problems = relationship('ContestProblem')
+    submissions = relationship('ProblemSubmission')
+
+
+class Problem(Base):
+    __tablename__ = 'problems'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    statement = Column(String)
+
+    tests = relationship('ProblemTest')
+
+
+class ContestProblem(Base):
+    __tablename__ = 'contests_problems'
+
+    contest_id = Column(Integer, ForeignKey('contests.id'), primary_key=True)
+    problem_id = Column(Integer, ForeignKey('problems.id'), primary_key=True)
+    problem_key = Column(String, primary_key=True)
+
+    contest = relationship('Contest')
+    problem = relationship('Problem')
+
+    #submissions = relationship('ProblemSubmission', foreign_keys=[contest_id, problem_id])
+
+    __table_args__ = (
+        UniqueConstraint('contest_id', 'problem_key'),
+    )
+
+
+class ProblemTest(Base):
+    __tablename__ = 'tests'
+
+    id = Column(Integer, primary_key=True)
+    problem_id = Column(Integer, ForeignKey('problems.id'))
+    input = Column(LargeBinary)
+    output = Column(LargeBinary)
+
+
+class ProblemSubmission(Base):
+    __tablename__ = 'problems_submissions'
+
+    id = Column(Integer, primary_key=True)
+    contest_id = Column(Integer, ForeignKey('contests.id'))
+    problem_id = Column(Integer, ForeignKey('problems.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    results = Column(JSON, nullable=True)
+
+    submitted_at = Column(DateTime(timezone=True))
+
+    source = Column(LargeBinary)
 
 
 class ContestMember(Base):
