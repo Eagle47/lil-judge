ce8d22527c7432e62226f30050c0935ae53df260
Alexey Ermakov <fufler@gmail.com>
2020-03-24 12:07:05 +0300

    Return list of failed/passed tests

--- problem_checker/__init__.py
+++ problem_checker/__init__.py
@@ -3,17 +3,19 @@ import sys
 
 
 def check(solution, tests):
+    tests_results = []
     for (test_input, test_output) in tests:
         with open(test_input) as inf:
             result = subprocess.run([sys.executable, solution], stdout=subprocess.PIPE, stdin=inf)
 
             if result.returncode != 0:
-                return False
+                tests_results.append(False)
 
             with open(test_output) as outf:
                 expected_output = outf.read().strip()
 
-                if result.stdout.decode().strip() != expected_output:
-                    return False
+                tests_results.append(
+                    result.stdout.decode().strip() == expected_output
+                )
 
-    return True
+    return tests_results
